678cb871e13b3a29355633827b108958eab09e61
Hal Fulcher <hallam.fulcher@gmail.com>
2021-07-11 15:24:56 +0100

    Import fonts

--- App.tsx
+++ App.tsx
@@ -1,36 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
 import BarCard from './BarCard';
+import { useFonts, ShareTechMono_400Regular } from '@expo-google-fonts/share-tech-mono';
+
+
 
 export default function App() {
+
+  let [fontsLoaded, error] = useFonts({
+    ShareTechMono_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } 
+
+
   return (
 
    <View style={styles.container}>
-      <Text style={styles.title}>Thirsty? These bars near you are still open:</Text>
-      <Text style={styles.title2}>You are searching postcode. Change location</Text>
+     <View style={styles.textborder}>
+      <Text style={{fontFamily: 'ShareTechMono_400Regular', fontWeight: 'bold',
+  fontSize: 17,
+  color: 'black'}}>
+  Thirsty? These bars near you are still open:</Text>
+      <Text style={styles.title2}>Change location</Text>
       <StatusBar style="auto" />
             <BarCard />      
+            </View>
     </View>
+    
 
   );
 }
 
+
 const styles = StyleSheet.create({
+
+
+  textborder: {
+top: 70
+  },
+ 
   container: {
     backgroundColor: '#F6F3FA',
-    // flex: 1,
-    // alignItems: 'center',
+    flex: 1,
+
+    alignItems: 'center',
+
     // justifyContent: 'center',   
 
   },
 
 title: {
-
+  
+  // fontFamily: 'ShareTechMono_400Regular',
 },
 
 title2: {
-
+color: '#19A6F5',
+fontSize: 12
 }
 
   
